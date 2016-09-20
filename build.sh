fc36782645c0cbe95123571bb0374bf2e0d3c61c
Ettore Di Giacinto <mudler@users.noreply.github.com>
2016-09-20 11:58:07 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -46,33 +46,41 @@ update_docker_companion() {
 
 check_python_version
 
-echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-binfmt:' > /proc/sys/fs/binfmt_misc/register || true
-
-update_docker_companion
-update_chroot sabayon/armhfp armhfp
-update_chroot sabayon/rpi-armhfp rpi
-update_chroot sabayon/bananapi-armhfp bananapi
-update_chroot sabayon/rpi-mc-armhfp rpi-mc
-update_chroot sabayon/odroid-x2-u2-armhfp odroid-x2-u2
-update_chroot sabayon/odroid-c2-armhfp odroid-c2
-#update_chroot sabayon/udooneo-armhfp udooneo
-
+# Check if binfmt is set
+[[ -f /proc/sys/fs/binfmt_misc/arm ]] && echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-binfmt:' > /proc/sys/fs/binfmt_misc/register || true
 
 export SABAYON_RELEASE="${SABAYON_RELEASE:-16}"
-echo "Release ${SABAYON_RELEASE}"
+echo "Building up Release ${SABAYON_RELEASE}"
 
+update_docker_companion
+
+update_chroot sabayon/armhfp armhfp
 # Doing chroot rootfs .tar.gz
 pushd sources/armhfp
-tar cpjf Sabayon_Linux_${SABAYON_RELEASE}_armv7l.tar.bz2 . && \
-md5sum Sabayon_Linux_${SABAYON_RELEASE}_armv7l.tar.bz2  > Sabayon_Linux_${SABAYON_RELEASE}_armv7l.tar.bz2.md5 && \ 
-mv Sabayon_Linux_${SABAYON_RELEASE}_armv7l.tar.bz2 ../../images/ && \
-mv Sabayon_Linux_${SABAYON_RELEASE}_armv7l.tar.bz2.md5 ../../images/
-
+	tar cpjf ../../images/Sabayon_Linux_${SABAYON_RELEASE}_armv7l.tar.bz2 .
 popd
 
+md5sum images/Sabayon_Linux_${SABAYON_RELEASE}_armv7l.tar.bz2  > images/Sabayon_Linux_${SABAYON_RELEASE}_armv7l.tar.bz2.md5 
+
+# Creating image for RPI
+update_chroot sabayon/rpi-armhfp rpi
 SABAYON_MOLECULE_HOME=$(pwd) molecule molecules/sabayon-arm-rpi-8G.spec
+
+# Creating image for RPI Media Center Edition
+update_chroot sabayon/rpi-mc-armhfp rpi-mc
 SABAYON_MOLECULE_HOME=$(pwd) molecule molecules/sabayon-arm-rpi-mc-8G.spec
+
+# Creating image for Odroid X2 U2 and U3
+update_chroot sabayon/odroid-x2-u2-armhfp odroid-x2-u2
 SABAYON_MOLECULE_HOME=$(pwd) molecule molecules/sabayon-arm-odroid-x2-u2-8G.spec
+
+# Creating image for Odroid C2
+update_chroot sabayon/odroid-c2-armhfp odroid-c2
 SABAYON_MOLECULE_HOME=$(pwd) molecule molecules/sabayon-arm-odroid-c2-8G.spec
+
+#update_chroot sabayon/udooneo-armhfp udooneo
 #SABAYON_MOLECULE_HOME=$(pwd) molecule molecules/sabayon-arm-udooneo-8G.spec
+
+# Creating image for BananaPi
+update_chroot sabayon/bananapi-armhfp bananapi
 SABAYON_MOLECULE_HOME=$(pwd) molecule molecules/sabayon-arm-bananapi-8G.spec
